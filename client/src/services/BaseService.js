5f0a6d9148349747eb273330d3b67a9f3c5fe518
thinhpham <phamthinh30522@gmail.com>
2024-03-12 22:32:44 +0700

    update code

--- client/src/services/BaseService.js
+++ client/src/services/BaseService.js
@@ -13,7 +13,7 @@ const host_API = process.env.REACT_APP_API_KEY
 const baseUrl = `${host_API}${appConfig.apiPrefix}${appConfig.apiVersion}`
 
 const BaseService = axios.create({
-    timeout: 5000,
+    timeout: 60000,
     baseURL: baseUrl,
     headers: {
         'Content-Type': 'application/json'
