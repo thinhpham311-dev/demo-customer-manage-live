86308ed457e5f8c0b3339a6c9f021d7ae9e21362
thinhpham <phamthinh30522@gmail.com>
2024-06-05 22:51:00 +0700

    pdate ode

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const app = require('./src/app');
+// const fs = require('fs')
+// const https = require('https');
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+});
+
+
+// https.createServer({
+//   key: fs.readFileSync("localhost-key.pem"),
+//   cert: fs.readFileSync("localhost.pem")
+// }, app).listen(port, () => {
+//   /* eslint-disable no-console */
+//   console.log(`Listening: https://localhost:${port}`);
+//   /* eslint-enable no-console */
+// });
